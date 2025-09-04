40ece1864e42aa4fbce58569f5d141e51fb576fa
Michael Zhan <michaelzhan2001@gmail.com>
2025-09-04 19:57:09 -0400

    scaffold endpoints

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,9 +10,26 @@ import (
 func main () {
 	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+	r.Route("/groups", func(r chi.Router) {
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {})
+		r.Delete("/{group_id}", func(w http.ResponseWriter, r *http.Request) {})
+
+		r.Get("/{group_id}/members", func(w http.ResponseWriter, r *http.Request) {})
+		r.Post("/{group_id}/members", func(w http.ResponseWriter, r *http.Request) {})
+		r.Patch("/{group_id}/members", func(w http.ResponseWriter, r *http.Request) {})
+		r.Delete("/{group_id}/members", func(w http.ResponseWriter, r *http.Request) {})
+
+		r.Post("/{group_id}/calculate", func(w http.ResponseWriter, r *http.Request) {})
 	})
 
-	http.ListenAndServe(":3000", r)
+	r.Route("/transactions", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {})
+		r.Post("/{transaction_id}", func(w http.ResponseWriter, r *http.Request) {})
+		r.Patch("/{transaction_id}", func(w http.ResponseWriter, r *http.Request) {})
+		r.Delete("/{transaction_id}", func(w http.ResponseWriter, r *http.Request) {})
+	})
+	
+	port := "3000"
+	fmt.Printf("Serving on port %s\n", port)
+	http.ListenAndServe(":" + port, r)
 }
