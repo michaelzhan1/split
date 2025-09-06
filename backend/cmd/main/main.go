2e6aef3c017c4ff5491a946e41e70748d8d6f2de
Michael Zhan <michaelzhan2001@gmail.com>
2025-09-06 08:36:25 -0400

    Scaffold db access

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 )
 
 func main () {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	conn, err := pgx.Connect(ctx, "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
 	r := chi.NewRouter()
 
 	r.Route("/groups", func(r chi.Router) {
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {})
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+
+			var name string
+			err = conn.QueryRow(ctx, "select name from member where id=$1", 1).Scan(&name)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.Write([]byte(name))
+		})
 		r.Delete("/{group_id}", func(w http.ResponseWriter, r *http.Request) {})
 
 		r.Get("/{group_id}/members", func(w http.ResponseWriter, r *http.Request) {})
