6644ca2d14a740aa30c63ef50eef932a8f58a859
Michael Zhan <michaelzhan2001@gmail.com>
2025-09-19 21:38:05 -0400

    set up path aliasing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+});
